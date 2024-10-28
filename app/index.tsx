647a19e5031acf9cb134cb228fb2cadd51c5c5a0
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-10-28 19:01:55 +0100

    Test of button in native env

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function MainPage() {
   return (
     <View style={styles.container}>
       <Text>Home Page</Text>
+      <TouchableOpacity>
+        <Text>Click here for magic</Text>
+      </TouchableOpacity>
     </View>
   );
 }
